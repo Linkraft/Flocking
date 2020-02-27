cc57b2b3ff606038e6cab8ddb6f74acb1b7197b9
Linkraft <36479127+Linkraft@users.noreply.github.com>
2020-02-27 02:08:22 -0500

    Finished the project! :)

--- StudentAI/Flock.cs
+++ StudentAI/Flock.cs
@@ -1,10 +1,8 @@
 ﻿using System.Collections.Generic;
 using AI.SteeringBehaviors.Core;
 
-namespace AI.SteeringBehaviors.StudentAI
-{
-    public class Flock
-    {
+namespace AI.SteeringBehaviors.StudentAI {
+    public class Flock {
         public float AlignmentStrength { get; set; }
         public float CohesionStrength { get; set; }
         public float SeparationStrength { get; set; }
@@ -13,14 +11,108 @@ namespace AI.SteeringBehaviors.StudentAI
         protected Vector3 AverageForward { get; set; }
         public float FlockRadius { get; set; }
 
-        #region TODO
         public Flock() {
             // Default constructor
+            AverageForward = new Vector3(0, 0, 0);
+            AveragePosition = new Vector3(0, 0, 0);
+
+            AlignmentStrength = 1;
+            CohesionStrength = 1;
+            SeparationStrength = 1;
+            FlockRadius = 50;
         }
-        public void Update(float deltaTime)
-        {
-            // Update goes here
+
+        public virtual void Update(float deltaTime) {
+            AverageForward = getAverageForward(Boids);
+            AveragePosition = getAveragePosition(Boids);
+
+            foreach(MovingObject boid in Boids) {
+                Vector3 accel = calcAlignmentAccel(boid);
+                accel += calcCohesionAccel(boid);
+                accel += calcSeparationAccel(boid);
+                accel *= boid.MaxSpeed * deltaTime;
+                boid.Velocity += accel;
+
+                if (boid.Velocity.Length > boid.MaxSpeed) {
+                    boid.Velocity.Normalize();
+                    boid.Velocity *= boid.MaxSpeed;
+                }
+
+                boid.Update(deltaTime);
+            }
+
         }
-        #endregion
+
+        private Vector3 getAverageForward(List<MovingObject> boids) {
+            int numBoids = boids.Count;
+            Vector3 average = new Vector3(0, 0, 0);
+            foreach (MovingObject boid in boids) {
+                average += boid.Velocity;
+            }
+            average /= numBoids;
+            return average;
+        }
+
+        private Vector3 getAveragePosition(List<MovingObject> boids) {
+            int numBoids = boids.Count;
+            Vector3 average = new Vector3(0, 0, 0);
+            foreach (MovingObject boid in boids) {
+                average += boid.Position;
+            }
+            average /= numBoids;
+            return average;
+        }
+
+
+        private Vector3 calcAlignmentAccel(MovingObject boid) {
+            Vector3 accel = AverageForward / boid.MaxSpeed;
+
+            if (accel.Length > 1) {
+                accel.Normalize();
+            }
+
+            return accel * AlignmentStrength;
+        }
+
+        private Vector3 calcCohesionAccel(MovingObject boid) {
+            Vector3 accel = AveragePosition - boid.Position;
+            float distance = accel.Length;
+            accel.Normalize();
+
+            // Set speed based on distance from flock
+            if (distance < FlockRadius) {
+                accel *= distance / FlockRadius;
+            }
+
+            return accel * CohesionStrength;
+        }
+
+        private Vector3 calcSeparationAccel(MovingObject boid) {
+            Vector3 sum = new Vector3(0, 0, 0);
+
+            foreach (MovingObject sibling in Boids) {
+                if (sibling == boid)
+                    continue;
+
+                Vector3 accel = boid.Position - sibling.Position;
+                float distance = accel.Length;
+                float safeDistance = boid.SafeRadius + sibling.SafeRadius;
+
+                // If a collision is likely...
+                if (distance < safeDistance) {
+                    // Scale vector according to how close boids are
+                    accel.Normalize();
+                    accel *= (safeDistance - distance) / safeDistance;
+                    sum += accel;
+                }
+            }
+
+            if (sum.Length > 1) {
+                sum.Normalize();
+            }
+
+            return sum * SeparationStrength;
+        }
+
     }
 }
